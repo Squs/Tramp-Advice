307a14d70b7ddf398dcf484a34fe927aad1a0fe7
ankake1022 <58929421+ankake1022@users.noreply.github.com>
2020-01-09 14:49:02 +0900

    Add files via upload

---
+++ src/sources/js/Custom.js
@@ -0,0 +1,8 @@
+//fadeoutアニメーション
+function fadeout(){
+    var sampleElement = document.getElementById("btn");
+    var list = sampleElement.classList;
+    console.log(list);
+
+    return true;
+}
